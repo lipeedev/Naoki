a1f25ca45edcb1e3524372ee5dbd895553142811
1Leep <anamariafakezaum@gmail.com>
2022-08-28 13:47:50 -0300

    fix: prefix commands system

--- src/client/events/client/messageCreate.js
+++ src/client/events/client/messageCreate.js
@@ -12,25 +12,25 @@ export default class MessageCreateEvent extends Event {
     }
     /** @param {Message} message */
     async runEvent(message) {
-        if (message.author.bot || !message.guildId) return;
+        if (message.author.bot || !message.guild) return;
 
         const guild = await this.client.getData(message.guild.id, 'guild');
-        const prefix = guild.prefix;
+        const { prefix } = guild;
 
         t = await this.client.getTranslate(message.guild.id);
         if (message.content.match(ClientMention(this.client.user.id))) return message.reply(t('client:mentioned', { 'author-tag': message.author.tag, prefix: prefix }));
         if (!message.content.toLowerCase().startsWith(prefix?.toLowerCase())) return;
 
-        const args = message.content.slice(prefix?.length)
-            .trim().split(/ +/g);
+        const args = message.content.slice(prefix?.length).trim().split(/ +/g);
         const cmd = args.shift().toLowerCase();
-        const command = this.client.commands.vanilla.filter(cmd => cmd.options.cmdType === 'vanilla').get(cmd) || this.client.commands.vanilla.find(cmd => cmd.options.aliases?.includes(cmd));
+
+        const command = this.client.commands.vanilla.get(`${cmd}-prefix`) || this.client.commands.vanilla.find(cmd => cmd.options.aliases?.includes(cmd));   
         if (!command) return;
 
         try {
-            const cmdD = await this.client.database.commands.findOne({ cmdName: command.name });
+            const cmdD = await this.client.database.commands.findOne({ cmdName: command.options.name });
             if (!cmdD) {
-                await this.client.database.commands.create({ cmdName: command.name });
+                await this.client.database.commands.create({ cmdName: command.options.name });
                 command.runCommand({ message, args }, t);
 
                 return;
