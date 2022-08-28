35cf8fd7061e4255a97801efb61ecfc56894eb16
rtsuki <haskgb3@gmail.com>
2022-08-27 22:54:26 -0300

    fix: a silly test error

--- index.js
+++ index.js
@@ -1,8 +1,2 @@
 import { NaokiClient } from './src/NaokiClient.js';
 (new NaokiClient()).start();
-
-import { transform } from '@babel/core';
-
-transform('code', {
-    presets: ['@babel/preset-env']
-});
