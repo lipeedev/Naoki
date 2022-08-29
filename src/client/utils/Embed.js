633ea120fd155fbc6f81471d46373822b5ce0e24
eloilsonfontenele2@gmail.com <eloilsonfontenele2@gmail.com>
2022-08-29 15:10:41 -0300

    refactor: improve code

--- src/client/utils/Embed.js
+++ src/client/utils/Embed.js
@@ -1,11 +1,14 @@
-import pkg from 'discord.js';
-const { EmbedBuilder, EmbedData, User } = pkg;
+import { EmbedBuilder, EmbedData, User } from 'discord.js';
 
 export default class DefaultEmbed extends EmbedBuilder {
-    /** @param {User} user @param {EmbedData} data */
-    constructor(user, data = {}) {
-        super(data);
-        this.setFooter({ text: user.tag, iconURL: user.displayAvatarURL() });
-        this.setColor('Yellow');
-    }
+  /**
+   * @param {User} user
+   * @param {EmbedData} data
+   */
+  constructor(user, data = {}) {
+    super(data);
+    this.setFooter({ text: user.tag, iconURL: user.displayAvatarURL() });
+    this.setColor('Yellow');
+  }
 }
+  
