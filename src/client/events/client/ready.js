a38021c1d823ce9f64bfe5e3ccfedf7081df931f
rtsuki <haskgb3@gmail.com>
2022-09-02 23:26:45 -0300

    feat: post stats to void bots

--- src/client/events/client/ready.js
+++ src/client/events/client/ready.js
@@ -1,4 +1,5 @@
 import Event from '../../../structures/ClientEventStructure.js';
+import VoidBots from 'voidbots';
 
 export default class ReadyEvent extends Event {
     constructor(client) {
@@ -10,5 +11,11 @@ export default class ReadyEvent extends Event {
     async runEvent() {
         await this.client.application.commands.set(this.client.commands.vanilla.filter(cmd => cmd.type == 'application').map(cmd => cmd.options)).then(cmd => this.client.logger('Discord, Application', `Posted ${cmd.size} commands to Discord.`)).catch(e => console.log(e));
         this.client.logger('Discord, Client', 'Connected successfully in ' + this.client.user.tag);
+        
+        const VoidManager = new VoidBots(process.env.VOID_BOTS_TOKEN, { autoPost: true }, this.client);
+        VoidManager.postStats(this.client.guilds.cache.size, this.client.shard.count);
+
+        process.on('unhandledRejection', (err) => console.log(err));
+        process.on('uncaughtException', (err) => console.log(err));
     }
 }
